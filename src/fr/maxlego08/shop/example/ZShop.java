49bb588905ff1e76919069f4005ddd4b0e22cbcc
Maxlego08 <maxlego08@sfr.fr>
2020-04-24 19:06:07 +0200

    first commit

---
+++ src/fr/maxlego08/shop/example/ZShop.java
@@ -0,0 +1,7 @@
+package fr.maxlego08.shop.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ZShop extends JavaPlugin{
+
+}
---
+++ src/fr/maxlego08/shop/zshop/factories/ShopItem.java
@@ -0,0 +1,99 @@
+package fr.maxlego08.shop.zshop.factories;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import fr.maxlego08.shop.zshop.items.Economy;
+
+public interface ShopItem{
+
+	/**
+	 * 
+	 * @return
+	 */
+	public ShopType getType();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getCategory();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getSlot();
+	
+	/**
+	 * 
+	 * @param player
+	 * @param amount
+	 */
+	public void performBuy(Player player, int amount);
+	
+	/**
+	 * 
+	 * @param player
+	 * @param amount
+	 */
+	public void performSell(Player player, int amount);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ItemStack getItem();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ItemStack getDisplayItem();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public double getSellPrice();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public double getBuyPrice();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getMaxStackSize();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean useConfirm();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Economy getEconomyType();
+	
+	/**
+	 * 
+	 * @param slot
+	 */
+	void setSlot(int slot);
+	
+	public enum ShopType{
+		
+		ITEM,
+		ITEM_SLOT,
+		UNIQUE_ITEM,
+		
+	}
+
+	
+}
