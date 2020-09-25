82a9a5c8dd0cf08026094143faa3bb76c35fc4e9
Maxlego08 <maxlego08@sfr.fr>
2020-09-25 21:35:45 +0200

    Update API

--- src/fr/maxlego08/shop/api/enums/Economy.java
+++ src/fr/maxlego08/shop/api/enums/Economy.java
@@ -3,6 +3,7 @@ package fr.maxlego08.shop.api.enums;
 import org.bukkit.Bukkit;
 
 import fr.maxlego08.shop.api.events.EconomyCurrencyEvent;
+import fr.maxlego08.shop.api.events.EconomyDenyEvent;
 import fr.maxlego08.shop.save.Lang;
 
 public enum Economy {
@@ -16,6 +17,8 @@ public enum Economy {
 	MYSQLTOKEN,
 
 	CUSTOM,
+
+	LEVEL,
 
 	ICECORE,
 
@@ -47,12 +50,37 @@ public enum Economy {
 			return Lang.currencyMySQLToken;
 		case ICECORE:
 			return Lang.currencyIceToken;
+		case LEVEL:
+			return Lang.currencyLevel;
 		case CUSTOM:
 			EconomyCurrencyEvent event = new EconomyCurrencyEvent();
 			Bukkit.getPluginManager().callEvent(event);
 			return event.getCurrency();
 		default:
 			return "$";
+		}
+	}
+
+	public String getDenyMessage() {
+		switch (this) {
+		case CUSTOM:
+			EconomyDenyEvent event = new EconomyDenyEvent();
+			Bukkit.getPluginManager().callEvent(event);
+			return event.getMessage();
+		case ICECORE:
+			return Lang.notEnouhtMoneyIceToken;
+		case LEVEL:
+			return Lang.notEnouhtMoneyLevel;
+		case MYSQLTOKEN:
+			return Lang.notEnouhtMoneyMySQLToken;
+		case PLAYERPOINT:
+			return Lang.notEnouhtMoneyPlayerPoint;
+		case TOKENMANAGER:
+			return Lang.notEnouhtMoneyTokenManager;
+		case VAULT:
+			return Lang.notEnouhtMoneyVault;
+		default:
+			return Lang.notEnouhtMoneyVault;
 		}
 	}
 
